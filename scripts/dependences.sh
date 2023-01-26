a677393e515c77867f8505650e6750fbc930ef85
Jonathan Abrantes <54941941+jonathanabrantes@users.noreply.github.com>
2023-01-26 09:45:55 -0300

    Update and rename kubectlinstall.sh to dependences.sh

---
+++ scripts/dependences.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Setup for Jenkins
+sudo apt-get update
+
+sudo apt-get install -y ca-certificates curl
+
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+
+apt-get install -y kubectl openjdk-17-jdk
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install jenkins -y
+
+curl -fsSL https://get.docker.com | sh
+
+curl -fsSL https://get.docker.com | sh
